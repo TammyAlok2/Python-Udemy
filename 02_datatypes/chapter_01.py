aeaaeee1f62ab71fdf8ebbbad0969f5fcaf6d20b
Alok Tamrakar <109581769+TammyAlok2@users.noreply.github.com>
2025-09-07 18:09:23 +0530

    feat : Satabadi 1 (07/09/25 ) - Datatypes : Numbers Done

--- 02_datatypes/chapter_01.py
+++ 02_datatypes/chapter_01.py
@@ -8,3 +8,9 @@ print(f"Id of 10: {id(10)}")
 print(f"Id of 12: {id(12)}")
 
 # Both id are different, so we can say that numbers are immutable 
+sugar  =1 
+print ("sugar intialize with ",sugar)
+print(f"id of 1 : {id(1)}")
+sugar = 2 
+print(f"id of 2 : {id(2)}")
+print ("sugar finalize  with ",sugar)
---
+++ 02_datatypes/chapter_03.py
@@ -0,0 +1,37 @@
+# Numbers : 
+# Numbers : Integers , Boolean , Real : floating - > decimal , Complex Numbers 
+
+
+#Integers : 
+black_tea_grams = 14 
+ginger_tea_grams = 15 
+
+total_grams = black_tea_grams + ginger_tea_grams
+
+print(f" Total grams of tea",total_grams)
+
+milk_liters = 10 
+servings  =5 
+
+milk_per_servings = milk_liters/servings;
+print(f"Milk servings is ",milk_per_servings)
+
+total_tea_bags = 7 
+pots = 4 
+
+bags_per_pot = total_tea_bags// pots # gives integer output in division 
+print(f"While tea bags per pot : {bags_per_pot}")
+
+
+total_cadmon_pods = 10 
+pods_per_cup = 3  
+left_over_pods = total_cadmon_pods %  pods_per_cup # gives remainder 
+
+print(f"Left over C pods {left_over_pods}")
+
+
+base_flavor_strength  = 2 
+scale_factor = 3 
+powerful_flavor = base_flavor_strength ** scale_factor # gives exponential answer , power 
+
+print(f"powerful flavor factor {powerful_flavor}")
